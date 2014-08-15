4be58f07ab651c7ef81b3a2269f45e4d09e94b2b
Bommer <quangthi_90@yahoo.com.vn>
2014-08-15 14:04:04 +0700

    - Setup OpenCart to develop
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

---
+++ docs/backup/admin-config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Ecommerce/admin/');
+define('HTTP_CATALOG', 'http://localhost/Ecommerce/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Ecommerce/admin/');
+define('HTTPS_CATALOG', 'http://localhost/Ecommerce/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\Program\xampp\htdocs\Ecommerce/admin/');
+define('DIR_SYSTEM', 'F:\Program\xampp\htdocs\Ecommerce/system/');
+define('DIR_DATABASE', 'F:\Program\xampp\htdocs\Ecommerce/system/database/');
+define('DIR_LANGUAGE', 'F:\Program\xampp\htdocs\Ecommerce/admin/language/');
+define('DIR_TEMPLATE', 'F:\Program\xampp\htdocs\Ecommerce/admin/view/template/');
+define('DIR_CONFIG', 'F:\Program\xampp\htdocs\Ecommerce/system/config/');
+define('DIR_IMAGE', 'F:\Program\xampp\htdocs\Ecommerce/image/');
+define('DIR_CACHE', 'F:\Program\xampp\htdocs\Ecommerce/system/cache/');
+define('DIR_DOWNLOAD', 'F:\Program\xampp\htdocs\Ecommerce/download/');
+define('DIR_LOGS', 'F:\Program\xampp\htdocs\Ecommerce/system/logs/');
+define('DIR_CATALOG', 'F:\Program\xampp\htdocs\Ecommerce/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'Ecommerce');
+define('DB_PREFIX', '');
+?>
---
+++ docs/backup/root-config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Ecommerce/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Ecommerce/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\Program\xampp\htdocs\Ecommerce/catalog/');
+define('DIR_SYSTEM', 'F:\Program\xampp\htdocs\Ecommerce/system/');
+define('DIR_DATABASE', 'F:\Program\xampp\htdocs\Ecommerce/system/database/');
+define('DIR_LANGUAGE', 'F:\Program\xampp\htdocs\Ecommerce/catalog/language/');
+define('DIR_TEMPLATE', 'F:\Program\xampp\htdocs\Ecommerce/catalog/view/theme/');
+define('DIR_CONFIG', 'F:\Program\xampp\htdocs\Ecommerce/system/config/');
+define('DIR_IMAGE', 'F:\Program\xampp\htdocs\Ecommerce/image/');
+define('DIR_CACHE', 'F:\Program\xampp\htdocs\Ecommerce/system/cache/');
+define('DIR_DOWNLOAD', 'F:\Program\xampp\htdocs\Ecommerce/download/');
+define('DIR_LOGS', 'F:\Program\xampp\htdocs\Ecommerce/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'Ecommerce');
+define('DB_PREFIX', '');
+?>
