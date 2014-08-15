f06582c088902046b8321ab16aec7432b8e88b85
Bommer <quangthi_90@yahoo.com.vn>
2014-08-15 23:15:33 +0700

    - Test extracter
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
+include 'simple_html_dom.php';
+
+// Create DOM from URL or file
+$html = file_get_html('http://www.google.com/');
+
+// Find all images 
+foreach($html->find('img') as $element) 
+       echo $element->src . '<br>';
+
+// Find all links 
+foreach($html->find('a') as $element) 
+       echo $element->href . '<br>';
+
+exit;
+
 // Version
 define('VERSION', '1.5.6.4');
 
