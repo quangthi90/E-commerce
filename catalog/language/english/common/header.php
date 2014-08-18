26558d4a0def3ae6b87327c1a4910ecb0d5ab0dd
tuyetphale993 <tuyetphale993@gmail.com>
2014-08-18 17:13:13 +0700

    - update menu
    Signed-off-by: tuyetphale993 <tuyetphale993@gmail.com>

--- catalog/language/english/common/header.php
+++ catalog/language/english/common/header.php
@@ -4,8 +4,18 @@ $_['text_home']           = 'Home';
 $_['text_wishlist']       = 'Wish List (%s)';
 $_['text_shopping_cart']  = 'Shopping Cart';
 $_['text_search']         = 'Nhập từ khóa tìm kiếm...';
-$_['text_welcome']        = '<a oncleanup="simpleTipLoginOnCleanup()" onshow="simpleTipLoginOnShow()" addclass="header_bar_simple_tip" hidearrow="1" rel="#header_login" href="javascript:;" class="simple_tip icon user">Đăng nhập</a>';  //<a href="%s">Đăng nhập</a> or <a href="%s">create an account</a>.
-$_['text_logged']         = '<span class="glyphicon glyphicon-star"></span><a class="simple_tip icon user" addclass="header_bar_simple_tip" hidearrow="1" js="loadLoggedInformation()" rel="nofollow" href="javascript:;">%s</a>'; //'You are logged in as <a href="%s">%s</a> <b>(</b> <a href="%s">Logout</a> <b>)</b>'
+$_['text_welcome']        = '
+	<a oncleanup="simpleTipLoginOnCleanup()" onshow="simpleTipLoginOnShow()" addclass="header_bar_simple_tip" hidearrow="1" rel="#header_login" href="%s" class="simple_tip icon user">Đăng nhập</a>
+	<a addclass="header_bar_simple_tip header_bar_drop_link" hidearrow="1" rel="#header_register" target="_blank" href="%s" class="simple_tip icon register">Đăng ký</a>
+';
+$_['text_logged']         = '
+	<a class="simple_tip icon post" addclass="header_bar_simple_tip header_bar_drop_link" hidearrow="1" rel="#header_post" href="javascript:;">Đăng tin</a>
+	<div id="header_post" class="hidden">
+	<a rel="nofollow" target="_blank" href="/home/post.php">Đăng sản phẩm</a>
+	<a rel="nofollow" target="_blank" href="/raovat/post.php">Đăng rao vặt</a>
+	<a rel="nofollow" target="_blank" href="/hoidap/post.php">Đăng hỏi đáp</a>
+	</div>
+	<a class="simple_tip icon user" addclass="header_bar_simple_tip" hidearrow="1" js="loadLoggedInformation()" rel="nofollow" href="%s">%s</a>';	 
 $_['text_account']        = 'My Account';
 $_['text_checkout']       = 'Checkout';
 ?>
